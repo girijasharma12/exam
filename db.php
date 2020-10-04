b8d05bfe4ac62ab07f8fbb82b550cbdfd95c1a22
Sourabh Somani <sourabh_somani2010@hotmail.com>
2020-10-04 23:48:08 +0530

    Cred Updated

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "sql12.freemysqlhosting.net";
-$username = "sql12362372";
+$username = "sql12368794";
 $password = "S7L7tWGZAL";
-$db = "sql12362372";
+$db = "sql12368794";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
